79309e148da4aab0d09e04415fe31d049b1d9008
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-01-14 09:10:21 +0200

    Rename folder, fix dataStorageLayer

---
+++ 01_recapJsonExpressServer/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const {port, host, activeStorage} = require('./mainServerConfig.json');
+const baseDir = __dirname;
+
+const {storage, storageLibraries} = require(path.join(baseDir,activeStorage));
+const {createDataStorage} = 
+    require(path.join(baseDir, storageLibraries.folder,storageLibraries.dataLayer));
+
+const dataStorage = createDataStorage(baseDir, {storage, storageLibraries});
+
+const server = http.createServer(app);
+
+app.use(express.json());
+app.use(cors());// webpage and api are launched from different origins, cross origin resource sharing needed
+
+// get all books
+app.get('/api/books', (req,res)=> dataStorage.getAll().then(result=>res.json(result)));
+
+app.get('/api/books/:id', (req,res)=>{
+    const bookId = req.params.id;
+    dataStorage.get('bookID',bookId)
+        .then(book=>res.json(book))
+        .catch(error => res.json(error));
+});
+
+server.listen(port,host, ()=>console.log(`Server ${host} is serving at port ${port}`));
---
+++ 01_recapJsonExpressServer/libraries/dataStorageLayer.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const path = require('path');
+
+function createDataStorage(baseDir, config) {
+  const libPath = path.join(baseDir, config.storageLibraries.folder);
+  const { CODES, MESSAGES } = require(path.join(libPath, config.storageLibraries.errorCodes));
+  const { initLayerFunctions } = require(path.join(libPath, config.storageLibraries.layerFunctions));
+  const { getAllFromStorage, getFromStorage } = initLayerFunctions(baseDir, config.storage);
+  
+  class Datastorage {
+    // getter (Example of using getter in testClass.js):
+    get CODES() {
+      return CODES;
+    }
+
+    // methods:
+    getAll() {
+      return getAllFromStorage();
+    }
+    get(key, value) {
+      return new Promise(async (resolve, reject) => {
+        if(!value) {
+          reject(MESSAGES.NOT_FOUND(`<empty ${key}>`));
+        }
+        else {
+          const result = await getFromStorage(key, value);
+          if(result) {
+            resolve(result);
+          }
+          else {
+            reject(MESSAGES.NOT_FOUND(value));
+          }
+        }
+      })
+    }
+  }
+  // end of class
+  return new Datastorage();
+}
+
+module.exports = { createDataStorage };
+
