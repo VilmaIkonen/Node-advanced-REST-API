3941bb0f223c5f490b05418a6211f7bb54b31c8c
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-01-26 14:03:55 +0200

    Second version of index and alternative text if no data available

--- 09_vantaaJobsLocalDataAndRefresh/indexV2.js
+++ 09_vantaaJobsLocalDataAndRefresh/indexV2.js
@@ -14,7 +14,7 @@ const app = express();
 
 const server = http.createServer(app);
 
-const dataPath = 'vantaaData.json';
+const dataPath = 'vantaaDatax.json';
 
 function checkUpdate() {
   // here some code to check if it is time to update local data
@@ -25,22 +25,20 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'vantaaJobsHome.htm
 
 // when fetching data from local datafile
 app.get('/json', async (req, res) => {
-  if(checkUpdate()) {
-    try {
-      const result = await fetch('http://gis.vantaa.fi/rest/tyopaikat/v1');
-      const data = await result.json();
-      await writeStorage(dataPath, data);
-      res.json(data);
-      console.log('Updated'); // For debugging. If in upeer part return true --> console: 'Updated', meaning it gets the data from Vantaa API
-    } 
-    catch (error) {
-    res.sendStatus = 404;   
+    if(checkUpdate()) {
+      try {
+        const result = await fetch('http://gis.vantaa.fi/rest/tyopaikat/v1');
+        const data = await result.json();
+        await writeStorage(dataPath, data);
+        console.log('Updated'); // For debugging. If in upeer part return true --> console: 'Updated', meaning it gets the data from Vantaa API
+      }
+      catch(err){
+        console.log('Update failed', err.message);
+        res.sendStatus = 404;
+      }
     }
-  }
-  else {
   const storage = await readStorage(dataPath);
   res.json(storage)
-  }
-})
+});
 
 server.listen(port, host, () => console.log(`Server ${host} running at port ${port}`))
