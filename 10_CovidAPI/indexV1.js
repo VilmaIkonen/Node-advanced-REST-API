c31999a313a7768ab5d6cf950613a168a37c5176
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-01-27 14:04:00 +0200

    Daily line chart added

--- 10_CovidAPI/indexV1.js
+++ 10_CovidAPI/indexV1.js
@@ -11,15 +11,18 @@ const { port, host } = require('./config.json');
 const app = express();
 const server = http.createServer(app);
 
+// Webpage paths:
 const homePath = path.join(__dirname, 'home.html');
 const cumulativeBarsPath = path.join(__dirname, 'cumulativeBars.html');
 const dailyBarsPath = path.join(__dirname, 'dailyBars.html');
+const dailyLinePath = path.join(__dirname, 'dailyLine.html')
 
 app.use(cors());
 
 app.get('/', (req, res) => res.sendFile(homePath));
 app.get('/cumulativebars', (req, res) => res.sendFile(cumulativeBarsPath));
-app.get('/dailybars', (req, res) => res.sendFile(dailyBarsPath))
+app.get('/dailybars', (req, res) => res.sendFile(dailyBarsPath));
+app.get('/dailyline', (req,res) => res.sendFile(dailyLinePath));
 
 // ###### Endpoints: ###### //
 
