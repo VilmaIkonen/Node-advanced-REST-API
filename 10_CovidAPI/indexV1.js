530ce0ff73f471ffa3ebfc296dde4c273ff4be86
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-01-27 13:06:47 +0200

    Cumulative and daily bar charts added

--- 10_CovidAPI/indexV1.js
+++ 10_CovidAPI/indexV1.js
@@ -12,13 +12,41 @@ const app = express();
 const server = http.createServer(app);
 
 const homePath = path.join(__dirname, 'home.html');
+const cumulativeBarsPath = path.join(__dirname, 'cumulativeBars.html');
+const dailyBarsPath = path.join(__dirname, 'dailyBars.html');
 
 app.use(cors());
 
 app.get('/', (req, res) => res.sendFile(homePath));
+app.get('/cumulativebars', (req, res) => res.sendFile(cumulativeBarsPath));
+app.get('/dailybars', (req, res) => res.sendFile(dailyBarsPath))
+
+// ###### Endpoints: ###### //
 
 app.get('/api/v1/data', async(req, res) => {
   const dateStrings = Object.keys(covidData.result) // keys in result are the dates in json file
   const data = dateStrings.map(date => covidData.result[date].confirmed) // get number of confirmed cases from the array
+
+  // another query type for data:
+  // const data = dateStrings.map(date => 
+  //   ({
+  //     date, 
+  //     confirmed: covidData.result[date]
+  //   })) 
+  
   res.json(data);
 })
+
+app.get('/api/v1/daily', async(req, res) => {
+  const dateStrings = Object.keys(covidData.result) // keys in result are the dates in json file
+  const data = dateStrings.map(date => covidData.result[date].confirmed) // get number of confirmed 
+
+  const dailyCases = [];
+  for (let i=0; i<data.length-1; i++) {
+    dailyCases.push(data[i+1]-data[i]); // next - previous data
+  }  
+  res.json(dailyCases);
+})
+
+
+server.listen(port, host, () => console.log(`Server running at ${host} in port ${port}`))
--- 10_CovidAPI/test.js
+++ 10_CovidAPI/test.js
@@ -3,9 +3,17 @@ const covidData = require('./FIN.json');
 const dateStrings = Object.keys(covidData.result) 
 const data = dateStrings.map(date => covidData.result[date].confirmed)  
 const data2 = Object.values(covidData.result);
-const tmp = data2.map(value => value.confirmed)
+const tmp = data2.map(value => value.confirmed);
 
 console.log(dateStrings);
 console.log(data);
 console.log(data2);
 console.log(tmp);
+
+const dailyCases = [];
+for (let i=0; i<data.length-1; i++) {
+  dailyCases.push(data[i+1]-data[i]); // next - previous data
+}
+console.log(dailyCases);  // --> data now in mixed order. Go through data and take max to calculate pixel ratio
+
+console.log(Math.max(...dailyCases));
