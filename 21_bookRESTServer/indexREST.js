faa1fea2c67c93c7e1295d0ed6f9df918c7263b6
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-03-03 10:01:00 +0200

    Comments added

--- 21_bookRESTServer/indexREST.js
+++ 21_bookRESTServer/indexREST.js
@@ -13,15 +13,17 @@ const {port, host, resource, idKey} = require('./configREST.json');
 const server = http.createServer(app);
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // for using json data (in webserver: urlencoded)
 
-// Routes:
+// ROUTES:
+// get and post operations need only books (here 'resource) in the route
 app.get(`/${resource}`, (req, res) => {
   db.getAll()
-  .then(result => res.json(result))
+  .then(result => res.json(result)) // return json
   .catch(err => res.json(err));
 });
 
+// delete and update operations use same route, also id (here 'value') needed
 app.route(`/${resource}/:value`)
 .get((req, res) => db.get(req.params.value)
   .then(result => res.json(result))
@@ -30,7 +32,7 @@ app.route(`/${resource}/:value`)
   .then(result => res.json(result))
   .catch(err => res.json(err)))
 .put((req, res) => {
-  if(!req.body) return res.sendStatus(400);
+  if(!req.body) return res.sendStatus(400); // if body/payload missing, status code sent
   if(req.body[idKey] == req.params.value){
     db.update(req.body)
     .then(result => res.json(result))
