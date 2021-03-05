622167ff44c108b04dd9e93a6fce85f12bbac599
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-03-05 12:24:21 +0200

    Rest server ready

---
+++ 23_endAssignmentGameDB/RESTserver/test.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const DataStorage = require('./dbStorage/dataStorageLayer');
+
+const db = new DataStorage();
+
+db.getAll().then(console.log).catch(console.log);
+db.get(1).then(console.log).catch(console.log);
+db.get(100).then(console.log).catch(console.log);
