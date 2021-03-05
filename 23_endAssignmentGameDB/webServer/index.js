142acb8076ff68293a4353524291285f80450d4e
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-03-05 13:03:43 +0200

    Webserver start, homepage and allpage working

--- 23_endAssignmentGameDB/webServer/index.js
+++ 23_endAssignmentGameDB/webServer/index.js
@@ -16,11 +16,11 @@ app.set('views', path.join(__dirname, 'pageviews'));
 app.use(express.static(path.join(__dirname, 'public'))); // access to public folder
 app.use(express.urlencoded({extended: false})); // for decoding the forms
 
-app.get('/', (req, res) => res.render('menu')); // If want to make manu.ejs generic, pass here data for 'resource', now resource = book (example in task 14)
+app.get('/', (req, res) => res.render('homepage')); 
 
 app.get('/all', async (req, res) => {
   try {
-    const result = await fetch('http://localhost:4000/books', {mode: 'cors'});
+    const result = await fetch('http://localhost:4000/api/games', {mode: 'cors'});
     const data = await result.json();
     res.render('allpage', {data});
    } 
@@ -29,122 +29,122 @@ app.get('/all', async (req, res) => {
   }
 });
 
-app.route('/getone')
-.get((req, res) => res.render('getform', {title: 'Get', header: 'Get', action: '/getone'}))
-.post(async(req, res) => {
-  try {
-    const bookID = req.body.bookID;
-    const result = await fetch(`http://localhost:4000/books/${bookID}`, {mode: 'cors'});
-    const data = await result.json();
-    res.render('bookpage', {data})
-  } 
-  catch (err) {
-    fetchError(res);
-  }
-});
+// app.route('/getone')
+// .get((req, res) => res.render('getform', {title: 'Get', header: 'Get', action: '/getone'}))
+// .post(async(req, res) => {
+//   try {
+//     const bookID = req.body.bookID;
+//     const result = await fetch(`http://localhost:4000/books/${bookID}`, {mode: 'cors'});
+//     const data = await result.json();
+//     res.render('bookpage', {data})
+//   } 
+//   catch (err) {
+//     fetchError(res);
+//   }
+// });
 
-app.route('/remove')
-.get((req, res) => res.render('getform', {title: 'Remove', header: 'Remove', action: '/remove'}))
-.post(async(req, res) => {
-  try {
-    const bookID = req.body.bookID;
-    const options = {method: 'DELETE', mode: 'cors'}; // no body/payload needed here as it oes through uri
-    const result = await fetch(`http://localhost:4000/books/${bookID}`, options);
-    const data = await result.json();
-    res.render('statuspage', {status: data})
-  } 
-  catch (err) {
-    fetchError(res);
-  }
-});
+// app.route('/remove')
+// .get((req, res) => res.render('getform', {title: 'Remove', header: 'Remove', action: '/remove'}))
+// .post(async(req, res) => {
+//   try {
+//     const bookID = req.body.bookID;
+//     const options = {method: 'DELETE', mode: 'cors'}; // no body/payload needed here as it oes through uri
+//     const result = await fetch(`http://localhost:4000/books/${bookID}`, options);
+//     const data = await result.json();
+//     res.render('statuspage', {status: data})
+//   } 
+//   catch (err) {
+//     fetchError(res);
+//   }
+// });
 
-app.get('/insertform', (req, res) => 
-  res.render('form', {
-    title: 'Add a book', 
-    header: 'Book data', 
-    action: '/insert', 
-    bookID: {value: '', readonly: ''},
-    name: {value: '', readonly: ''},
-    author: {value: '', readonly: ''},
-    type: {value: '', readonly: ''},
-    year: {value: '', readonly: ''}
-  })
-);
+// app.get('/insertform', (req, res) => 
+//   res.render('form', {
+//     title: 'Add a book', 
+//     header: 'Book data', 
+//     action: '/insert', 
+//     bookID: {value: '', readonly: ''},
+//     name: {value: '', readonly: ''},
+//     author: {value: '', readonly: ''},
+//     type: {value: '', readonly: ''},
+//     year: {value: '', readonly: ''}
+//   })
+// );
 
-app.post('/insert', async (req, res) => {
-  try {
-    const options = {
-      method: 'POST', 
-      body: JSON.stringify(req.body), 
-      mode: 'cors', 
-      headers: {'Content-Type': 'application/json'}
-    };
+// app.post('/insert', async (req, res) => {
+//   try {
+//     const options = {
+//       method: 'POST', 
+//       body: JSON.stringify(req.body), 
+//       mode: 'cors', 
+//       headers: {'Content-Type': 'application/json'}
+//     };
 
-    const result = await fetch('http://localhost:4000/books', options);
-    const data = await result.json();
-    res.render('statuspage', {status: data})
-  } 
-  catch (err) {
-    fetchError(res);
-  }
-});
+//     const result = await fetch('http://localhost:4000/books', options);
+//     const data = await result.json();
+//     res.render('statuspage', {status: data})
+//   } 
+//   catch (err) {
+//     fetchError(res);
+//   }
+// });
 
-app.get('/updateform', (req, res) => 
-  res.render('form', {
-    title: 'Update a book', 
-    header: 'Book data', 
-    action: '/updatedata', 
-    bookID: {value: '', readonly: ''},
-    name: {value: '', readonly: 'readonly'},
-    author: {value: '', readonly: 'readonly'},
-    type: {value: '', readonly: 'readonly'},
-    year: {value: '', readonly: 'readonly'}
-  })
-);
+// app.get('/updateform', (req, res) => 
+//   res.render('form', {
+//     title: 'Update a book', 
+//     header: 'Book data', 
+//     action: '/updatedata', 
+//     bookID: {value: '', readonly: ''},
+//     name: {value: '', readonly: 'readonly'},
+//     author: {value: '', readonly: 'readonly'},
+//     type: {value: '', readonly: 'readonly'},
+//     year: {value: '', readonly: 'readonly'}
+//   })
+// );
 
-app.post('/updatedata', async (req, res) => {
-  try {
-    const bookID = req.body.bookID;
-    const result = await fetch(`http://localhost:4000/books/${bookID}`, {mode: 'cors'});
-    const data = await result.json();
-    if(data.message) {
-      res.render('statuspage', {status: data}) // this if is needed because CODES/MESSAGES have 'type' and also the book db has filed 'type'
-    } 
-    else {
-      res.render('form', {
-        title: 'Update a book', 
-        header: 'Book data', 
-        action: '/update', 
-        bookID: {value: data.bookID, readonly: 'readonly'},
-        name: {value: data.name, readonly: ''},
-        author: {value: data.author, readonly: ''},
-        type: {value: data.type, readonly: ''},
-        year: {value: data.year, readonly: ''}
-      }) 
-    }   
-  } 
-  catch (err) {
-    fetchError(res);
-  }
-});
+// app.post('/updatedata', async (req, res) => {
+//   try {
+//     const bookID = req.body.bookID;
+//     const result = await fetch(`http://localhost:4000/books/${bookID}`, {mode: 'cors'});
+//     const data = await result.json();
+//     if(data.message) {
+//       res.render('statuspage', {status: data}) // this if is needed because CODES/MESSAGES have 'type' and also the book db has filed 'type'
+//     } 
+//     else {
+//       res.render('form', {
+//         title: 'Update a book', 
+//         header: 'Book data', 
+//         action: '/update', 
+//         bookID: {value: data.bookID, readonly: 'readonly'},
+//         name: {value: data.name, readonly: ''},
+//         author: {value: data.author, readonly: ''},
+//         type: {value: data.type, readonly: ''},
+//         year: {value: data.year, readonly: ''}
+//       }) 
+//     }   
+//   } 
+//   catch (err) {
+//     fetchError(res);
+//   }
+// });
 
-app.post('/update', async (req, res) => {
-  try {
-    const options = {
-      method: 'PUT', 
-      body: JSON.stringify(req.body), 
-      mode: 'cors', 
-      headers: {'Content-Type': 'application/json'}
-    };
+// app.post('/update', async (req, res) => {
+//   try {
+//     const options = {
+//       method: 'PUT', 
+//       body: JSON.stringify(req.body), 
+//       mode: 'cors', 
+//       headers: {'Content-Type': 'application/json'}
+//     };
 
-    const result = await fetch(`http://localhost:4000/books/${req.body.bookID}`, options);
-    const data = await result.json();
-    res.render('statuspage', {status: data})
-  } 
-  catch (err) {
-    fetchError(res);
-  }
-})
+//     const result = await fetch(`http://localhost:4000/books/${req.body.bookID}`, options);
+//     const data = await result.json();
+//     res.render('statuspage', {status: data})
+//   } 
+//   catch (err) {
+//     fetchError(res);
+//   }
+// })
 
 server.listen(port, host, () => console.log(`Server running on ${host}: ${port}`))
 
